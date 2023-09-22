300823eb553f209eaad5356e5c48ef9e5499322e
Yannick Nwankwo <117281275+Yannick-Nw@users.noreply.github.com>
2023-09-22 13:30:11 +0200

    R und i auslesen

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+//#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+/* Include Datei für getopt(); getopt.h geht auch, ist aber älter. */
+#include <unistd.h>
 
-int main(){
-    std::cout << "Hello myfind" << std::endl;
+int main(int argc, char *argv[]){
+    int opt = 0;
+    bool recursive = false;
+    bool caseInsensitive = false;
+
+    while ((opt = getopt(argc, argv, "Ri")) != EOF)
+    {
+        switch (opt)
+        {
+            case 'R':
+                recursive = true;
+                break;
+            case 'i':
+                caseInsensitive = true;
+                break;
+            default:
+                assert(0);
+        }
+    }
+
     return 0;
 }
