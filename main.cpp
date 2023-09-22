27a4f65c02cfa09f403d382d3fb61469d68c6f91
Yannick Nwankwo <117281275+Yannick-Nw@users.noreply.github.com>
2023-09-22 19:19:18 +0200

    auslesen von terminal befehl

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
-//#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-/* Include Datei für getopt(); getopt.h geht auch, ist aber älter. */
+//#include <stdlib.h>
+#include <vector>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
-    int opt = 0;
+int main(int argc, char* argv[]) {
+    std::vector<std::string> filenames;
+    std::string searchpath;
     bool recursive = false;
     bool caseInsensitive = false;
 
-    while ((opt = getopt(argc, argv, "Ri")) != EOF)
-    {
-        switch (opt)
-        {
+    int opt;
+    while ((opt = getopt(argc, argv, "Ri")) != -1) {
+        switch (opt) {
             case 'R':
                 recursive = true;
                 break;
@@ -21,9 +19,21 @@ int main(int argc, char *argv[]){
                 caseInsensitive = true;
                 break;
             default:
-                assert(0);
+                std::cerr << "Usage: " << argv[0] << " [-R] [-i] searchpath filename1 [filename2]...[filenameN]\n";
+                return 1;
         }
     }
 
+    if (optind < argc) {
+        searchpath = argv[optind++];
+        while (optind < argc) {
+            filenames.push_back(argv[optind++]);
+        }
+    }
+
+    find_files(searchpath, filenames, recursive, caseInsensitive);
+
+    //std::cout << recursive << caseInsensitive << "\n";
     return 0;
 }
+
